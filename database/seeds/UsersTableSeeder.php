9317c0d2edaaaf84a504757db0196bf2eb74c1a8
vagrant <vagrant@homestead>
2019-02-15 11:42:46 +0000

    Administrator can delete users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,8 +16,9 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'YunyiZhang';
+        $user->name = 'Yunyi Zhang';
         $user->email = 'zhangyunyig@gmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
